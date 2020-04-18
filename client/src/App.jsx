1e2b4ff0a4cd210ce434ca5ddbe88e5facd04ac0
Mason Borchard <masonborchard@gmail.com>
2020-04-18 16:04:36 -0700

    get rid of nested routes that will not be needed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import SearchBar from "./components/Search/SearchBar.jsx"
+import SearchBar from "./components/Search/SearchBar.jsx";
+import Home from "./components/Home.jsx";
+import Shows from "./components/Shows.jsx";
+import Contact from "./components/Contact.jsx";
+import About from "./components/Contact.jsx";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,86 +13,99 @@ import {
   useParams
 } from "react-router-dom";
 
-export default function App() {
-  return (
 
-    <Router>
-
+class App extends React.Component {
+  render() {
+    return (
       <div>
-        <ul>
-        <li>
-        <SearchBar/>
-        </li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/shows">Find Shows</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact Us</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/shows'>
-            <Shows />
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
-          <Route path='/contact'>
-            <Contact/>
-          </Route>
-        </Switch>
+        <h1>Home</h1>
       </div>
-    </Router>
-  );
+    )
+  }
 }
+export default App
 
-function Home() {
-  return <h2>Home</h2>;
-}
 
-function About() {
-  return <h2>About</h2>;
-}
+// export default function App() {
+//   return (
 
-function Shows() {
-  let match = useRouteMatch();
+//     <Router>
 
-  return (
-    <div>
-    <input />
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>one</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>
-            two
-          </Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path={`${match.path}/:artist`}>
-          <Shows />
-        </Route>
-        <Route path={match.path}>
-          <h3>Select a show</h3>
-        </Route>
-      </Switch>
-    </div>
-  );
-}
+//       <div>
+//         <ul>
+//         <li>
+//         <SearchBar/>
+//         </li>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/shows">Find Shows</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact">Contact Us</Link>
+//           </li>
+//         </ul>
 
-function Contact() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
-}
+//         <Switch>
+//           <Route path='/about'>
+//             <About />
+//           </Route>
+//           <Route path='/shows'>
+//             <Shows />
+//           </Route>
+//           <Route path='/'>
+//             <Home />
+//           </Route>
+//           <Route path='/contact'>
+//             <Contact/>
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+// function Shows() {
+//   let match = useRouteMatch();
+
+//   return (
+//     <div>
+//     <input />
+//       <ul>
+//         <li>
+//           <Link to={`${match.url}/components`}>one</Link>
+//         </li>
+//         <li>
+//           <Link to={`${match.url}/props-v-state`}>
+//             two
+//           </Link>
+//         </li>
+//       </ul>
+//       <Switch>
+//         <Route path={`${match.path}/:artist`}>
+//           <Shows />
+//         </Route>
+//         <Route path={match.path}>
+//           <h3>Select a show</h3>
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+
+// function Contact() {
+//   let { topicId } = useParams();
+//   return <h3>Requested topic ID: {topicId}</h3>;
+// }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx';
+import Shows from './components/Shows.jsx';
+import Contact from './components/Contact.jsx';
 
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/shows">Shows</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} />
+      <Route path="/shows" component={Shows} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    routing,
   document.getElementById('root')
 );
