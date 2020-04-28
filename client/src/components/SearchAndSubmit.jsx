f654d77af1cbd3bf4bc123828638192ddb6b433d
Mason Borchard <masonborchard@gmail.com>
2020-04-27 20:23:46 -0700

    algorithm progress

--- client/src/components/SearchAndSubmit.jsx
+++ client/src/components/SearchAndSubmit.jsx
@@ -28,25 +28,7 @@ class SearchAndSubmit extends React.Component {
 
   }
 
-
-  getTopSetlists(){
-    axios.get('/top-setlists')
-      .then((data) => this.setState({topSetlists: data.data}))
-      .then(() => {console.log('this.state.topSetlists: ', this.state.topSetlists)})
-      .catch((err) => {
-        console.log(err);
-        })
-  }
-
   calculateTopSetlist(){
-    var song1 = [];
-    var song2 = [];
-    var song3 = [];
-    var song4 = [];
-    var song5 = [];
-    var song6 = [];
-    var song7 = [];
-    var song8 = [];
     var result = {};
     // iterate over array of objects
     for (var i = 0; i < this.state.allSetlists.length; i++){
@@ -59,45 +41,89 @@ class SearchAndSubmit extends React.Component {
       }
     }
     // iterate over keys 0-7 in result obj
+    // var setObjBySong = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: []}
+    var setObjBySong = {};
     for (var key in result){
-      var objBySong = {};
-      // iterate over the array associate with each key in result (ie: first array is all choices for a first song)
-      for (var k = 0; k < result[key].length; k++){
-        // if entry is blank or anything falsy
-        console.log('result[key][k]', result[key][k])
-        if (result[key][k] === '' || !result[key][k]){
+      console.log('result[key]', result[key])
+      // this creates one persons setlist
+      // var setArr = result[key].join(',').split(', ');
+      var setArr = result[key];
+      console.log('setArr: should be one setlist', setArr)
+      // iterate over that persons setlist
+     var songAtNumArr = []
+      for (var k = 0; k < 8; k++){
+        // each key 1-8 will contain all songs by all fans at the position
+        setObjBySong[k].push(setArr[k]);
+        console.log('setObjBySong[k], should be an array with songs', setObjBySong[k]);
+        // if (setArr[k] === '' || !setArr[k]){
+        //   continue;
+        // }
+        // if the song is not already a key
+        // if (setObjBySong[k] === undefined){
+        //   // make a space for it and set to 1
+        //   setObjBySong[k] = arrayAtNum;
+        //   console.log('setObjBySong[k] should be a song', setObjBySong[k])
+        // }
+                // else add one to the song
+                // setObjBySong[setArr[k]]++;
+                // console.log('setObjBySong[setArr[k]] after adding ++', setObjBySong[setArr[k]])
+
+        }
+      }
+        for (var key in setObjBySong){
+          var songsAtPosition = {};
+          for (var x = 0; x < setObjBySong[key].length; x++){
+            var song = setObjBySong[key][x];
+        if (song === '' || !song){
           continue;
         }
-        // if the song is not already a key
-        if (objBySong[key][k] === undefined){
+        //if the song is not already a key
+        if (songsAtPosition[song] === undefined){
           // make a space for it and set to 1
-          objBySong[key][k] = 1;
-          console.log('objBySong', objBySong)
+          songsAtPosition[song] = 1;
+          console.log('songsAtPosition[song] should be a num', songsAtPosition[song]);
+          console.log('songsAtPosition obj', songsAtPosition);
         }
-        // else add one to the song
-        objBySong[key][k]++;
-        console.log('objBySong after adding ++', objBySong)
+                // else add one to the song
+                songsAtPosition[song]++;
+                console.log('songsAtPosition after adding ++', songsAtPosition)
+
+          }
+          var max = Object.values(songsAtPosition)[0];
+          console.log('Object.values(songsAtPosition)', Object.values(songsAtPosition));
+          var topSong = Object.keys(songsAtPosition)[0];
+          console.log('Object.keys(songsAtPosition)', Object.keys(songsAtPosition))
+          // iterate over object values in objBySong which are numbers
+          for (var m = 0; m < Object.values(songsAtPosition).length; m++){
+            // if song was chosen more than max
+            if (Object.values(songsAtPosition)[m] > max){
+              // it is now the max
+              max = Object.values(songsAtPosition)[m];
+              topSong = Object.keys(songsAtPosition)[m];
+            }
+
+
 
       }
-      // set max equal to first song by default
-      var max = Object.values(objBySong)[0];
-      var topSong = Object.keys(objBySong)[0];
-      // iterate over object values in objBySong which are numbers
-      for (var m = 0; m < Object.values(objBySong).length; m++){
-        // if song was chosen more than max
-        if (Object.values(objBySong)[m] > max){
-          // it is now the max
-          max = Object.values(objBySong)[m];
-          topSong = Object.keys(objBySong)[m];
-        }
-      }
-      // push topSong title into topSetlist array
       this.setState({
         topSetlist: this.state.topSetlist.push(topSong)
       })
-      console.log('current topSetList array', this.state.topSetlist)
+
+
     }
+
   }
+
+
+  // getTopSetlists(){
+  //   axios.get('/top-setlists')
+  //     .then((data) => this.setState({topSetlists: data.data}))
+  //     .then(() => {console.log('this.state.topSetlists: ', this.state.topSetlists)})
+  //     .catch((err) => {
+  //       console.log(err);
+  //       })
+  // }
+
 
   handleEntry(e) {
     e.preventDefault();
